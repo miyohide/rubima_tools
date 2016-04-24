9a7f87d359b4895821db51d92cc4a62cc37a9deb
miyohide <miyohide@gmail.com>
2016-04-24 22:23:21 +0900

    いろいろと対応

--- scripts/hiki2md.rb
+++ scripts/hiki2md.rb
@@ -1,26 +1,53 @@
+ISSUE_DATE = {
+  "0001" => "2004-09-10",
+  "0002" => "2004-10-16",
+  "0003" => "2004-11-15",
+  "0004" => "2004-12-17",
+  "0005" => "2005-02-15",
+  "0006" => "2005-05-09",
+  "0007" => "2005-06-19",
+  "0008" => "2005-07-19",
+  "0009" => "2005-09-06",
+  "0010" => "2005-10-10",
+  "0011" => "2005-11-16",
+  "0012" => "2005-12-23",
+  "0013" => "2006-02-20",
+  "0014" => "2006-05-15",
+  "0015" => "2006-07-13",
+  "0016" => "2006-09-20",
+  "0017" => "2006-11-26",
+  "0018" => "2007-02-28",
+  "0019" => "2007-05-18"
+}
+
 ARGV.each do |filename|
   lines = File.readlines(filename)
 
+  # 最初の行にはタイトルが入っているものと仮定
   first_line = lines.shift.chomp
+  # ファイル名の命名規則。最初の4文字は発行した号
   issue_num = filename[0, 4]
 
+  # Jekyll用のヘッダ
   headers = [ "---\n",
-    "layout: default\n",
+    "layout: post\n",
     "title: #{first_line}\n",
     "short_title: #{first_line}\n",
-    "tags: #{issue_num} backnumber\n",
-    "---\n\n",
-    "# #{first_line}\n\n"]
+    "tags: #{issue_num}\n",
+    "---\n\n"]
 
   body = lines.map { |line|
-    line.sub(/^(\!+)\s/) { '#'*($1.length + 1) + ' ' }.  ## イレギュラー対応。タイトルをh1にする
+    line.sub(/^(\!+)/) { '#'*($1.length + 1) + ' ' }.  ## イレギュラー対応。タイトルをh1にする
     sub(/^(\*)\s/) { '-'*($1.length) + ' ' }.
+    sub(/^\"\"\s/) { '> ' }.
+    sub(/^:([^:]+):(.+)$/) { '<dl><dt>' + $1 + '</dt><dd>' + $2 + '</dd></dl>' }.
     gsub(/\[\[([^|]+)\|([^\]]+)\]\]/) { '[' + $1 + '](' + $2 + ')' }.
     gsub(/'''([^']+)'''/) { '***' + $1 + '***' }
   }
 
   headers.concat(body)
 
-  md_filename = filename.sub(/\.hiki$/, '.md')
+  # Markdownファイルとして出力
+  md_filename = ISSUE_DATE[issue_num] + "-" + filename.sub(/\.hiki$/, '.md')
   open(md_filename, 'w') { |f| f.write(headers.join) }
 end
